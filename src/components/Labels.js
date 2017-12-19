3080200b0e0f80254bea34d6e3cedc2197f55219
ryan heupler <rheupler@gmail.com>
2017-12-19 15:03:53 -0500

    adds label updates

--- src/components/Labels.js
+++ src/components/Labels.js
@@ -3,19 +3,13 @@ import './Labels.css';
 
 const Labels = (props) => (
   <div className="labels">
-    <div className="label-type">
-      <p>What would you like to work with:</p>
-      <button value="Help wanted" className="label help-wanted button">Help Wanted</button>
-      <button value="bug" className="label bug button">Bug</button>
-      <button value="first-timers" className="label first-timers button">First-timers only</button>
-      <button value="good-first" className="label good-first button">Good first bug</button>
-    </div>
     <div className="label-language">
+      <button onClick={props.handleClick} value="CSS" className="label css button">CSS</button>
       <button onClick={props.handleClick} value="JavaScript" className="label javascript button">JavaScript</button>
       <button onClick={props.handleClick} value="Python" className="label javascript button">Python</button>
       <button onClick={props.handleClick} value="Java" className="label java button">Java</button>
-      <button onClick={props.handleClick} value="CSS" className="label css button">CSS</button>
-      <button onClick={props.handleClick} value="PHP" className="label css button">PHP</button>
+      <button onClick={props.handleClick} value="PHP" className="label php button">PHP</button>
+      <button onClick={props.handleClick} value="Ruby" className="label ruby button">Ruby</button>
     </div>
   </div>
 )
