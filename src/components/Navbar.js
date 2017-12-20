2de7a33e877ddd518cd0bb820e1b1f23d2e4b264
ryan heupler <rheupler@gmail.com>
2017-12-20 14:51:58 -0500

    adds navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => (
+  <div className="navbar">
+    <h1>Github Issue Finder</h1>
+    <p>Not sure how to start? <a href="https://codeburst.io/a-step-by-step-guide-to-making-your-first-github-contribution-5302260a2940">Here&#8217;s</a> a guide on making your first contribution</p>
+  </div>
+)
+
+export default Navbar;
